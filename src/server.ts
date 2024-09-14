58510f6c01c9a03425212f6769b823eab356aece
tDeven1993 <tdeven79@gmail.com>
2024-09-14 21:09:44 +0800

    update

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,16 @@
 ﻿import express from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors'; // 引入 cors
 import routes from './routes';
 
 const app = express();
+
+// 使用 cors 中间件
+app.use(cors({
+    origin: 'https://taskcenter.framer.website', // 允许的源地址
+    methods: 'GET,POST,PUT,DELETE,OPTIONS', // 允许的方法
+    allowedHeaders: 'Content-Type,Authorization' // 允许的请求头
+}));
 
 app.use(bodyParser.json());
 app.use(routes);
